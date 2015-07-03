1d4de8788b73327edeff06751e82b946dd3c2956
Peter Sobot <psobot@gmail.com>
2015-07-02 22:16:30 -0400

    welcome to the ｆｕｔｕｒｅ

--- Guardfile
+++
@@ -1,3 +0,0 @@
-guard 'livereload' do
-  watch(%r{public/.+\.(css|js|html)})
-end
--- source/javascripts/fancybuttons.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-  jQuery(document).on('pjax:beforeSend', function(e) {
-    jQuery('.active').removeClass('active');
-  });
-  jQuery(document).on('pjax:complete', function(e) {
-    jQuery(e.relatedTarget).addClass('active');
-  });
-});
